1625042bba628955b4bed1322a21d6a4f8a9ffcb
Emanuel Bravo <emanuelbravo0706@gmail.com>
2023-11-13 13:36:48 -0300

    remove lines, spaces and comments

--- pcc.py
+++ pcc.py
@@ -1,19 +1,44 @@
 import re
 import sys
 
-# Inventar nome irado = PreCCessor ??
+#This is the PreCCessor, a simple pre-processor for C language.
+#It will remove comments, linebreaks, multiple spaces and desnecessary spaces.
+#It will also expand #include and #define
 
-def remove_comments_linebreak(line, code):
-    oneSpaceLine = re.sub(r'(  +)', ' ', line)
 
-    slash_position = oneSpaceLine.find("//")
+def remove_inline_comments(line):
+
+    slash_position = line.find("//")
     if slash_position != -1:
-        code += oneSpaceLine[:slash_position]
-    elif oneSpaceLine.find('#include') == -1:
-        code += oneSpaceLine.replace('\n', ' ')
+        aux = line[:slash_position]
     else:
-        code += oneSpaceLine
-    return code
+        aux = line
+    return aux
+
+def remove_multiline_comments(code):
+    aux = code
+    return aux
+
+
+def remove_linebreak(code):
+    # destrincha linha por linha, checa #include or #define, se houver ele adiciona \n ao final da linha
+    aux = ''
+    for line in code.splitlines():
+        if line.find('#include') == -1 and line.find('#define') == -1:
+            aux += line
+        else:
+            aux += line + '\n'
+    return aux
+
+
+def remove_multiple_spaces(code):
+     aux = re.sub(r'(  +)', ' ', code) # remove multiple spaces
+     return aux
+
+def remove_desnecessary_space(code):
+    # aux = re.sub(r'([a-zA-Z0-9])(\s+)([a-zA-Z0-9])', r'\1\3', code) # remove spaces between letters and numbers
+    aux = re.sub(r'\s*([;:`\'|,!"(){}=\-<>\[\]])\s*', r'\1', code) # remove spaces after ;(){}=-
+    return aux
 
 def expand_include():
     return
@@ -39,23 +64,41 @@ if __name__ == '__main__':
 
     output_file = input_file[0:-2] + "-pp.c"
 
-
     print("preCcessor is running...")
-    print(input_file)
-    print(output_file)
-
     input = open(input_file, "r")
     output = open(output_file, "w")
 
+
     newCode = ''
     for line in input:
-        newCode = remove_comments_linebreak(line, newCode)
-        expand_define()
-        expand_include
+        line = remove_inline_comments(line)
+        newCode += line
+    newCode = remove_linebreak(newCode)
+    newCode = remove_multiple_spaces(newCode)
+    newCode = remove_desnecessary_space(newCode)
 
+    
+
+    expand_define()
+    expand_include()
     output.write(newCode)
 
     print("preCcessor finished with success!")
 
     input.close()
     output.close()
+
+
+
+
+
+'''
+    order:
+    1. import define, include and macros (recursive)
+    2. expand define and macros (recursive) 
+    3. remove inline comments
+    4. remove linebreaks
+    5. remove 'multiline' comments
+    6. remove multiple spaces
+    7. remove desnecessary spaces
+'''
