076971e7ccef08b6748288137aa1c5c62e43c7d7
Emanuel Bravo <emanuelbravo0706@gmail.com>
2023-11-20 10:25:24 -0300

    making the code _prettier_

--- pcc.py
+++ pcc.py
@@ -1,43 +1,9 @@
 import re
 import sys
 
-#This is the PreCCessor, a simple pre-processor for C language.
-#It will remove inline and multiline comments, linebreaks, multiple and/or desnecessary spaces.
-#It will also expand #include and #define
-
-def process_include_directives(text):
-    include_directives = re.compile(r'#include\s*[<](.*?)[>]').findall(text)    
-    include_locals = re.compile(r'#include\s*["](.*?)["]').findall(text)
-    included_any = False
-
-    for match in include_directives:
-        try:
-            filepath = '/usr/include/' + match
-            with open(filepath, 'r') as include_file:
-                include_content = include_file.read()
-                text = text.replace(f'#include <{match}>', include_content)
-                included_any = True
-                include_directives.remove(match)
-        except FileNotFoundError:
-            print(f"Warning: File '{match}' not found in include directive.")            
-
-
-    for match in include_locals:
-        try:
-            with open(match, 'r') as include_file:
-                include_locals.remove(match)
-                include_content = include_file.read()
-                included_any = True
-                text = text.replace(f'#include "{match}"', include_content)
-        except FileNotFoundError:
-            print(f"Warning: File '{match}' not found in local directory.")
-    
-    if (len(include_directives) + len(include_locals)) != 0 and included_any == False:
-        return text
-    else:
-        return process_include_directives(text)
-
-
+#This is the PreCCessor, a simple pre-processor for the C language.
+#It will remove inline and multiline comments, linebreaks, multiple and desnecessary spaces.
+#It will also expand the #include and #define
 
 def remove_inline_comments(code):
     aux = ''
@@ -84,6 +50,43 @@ def remove_desnecessary_space(code):
             aux += re.sub(r'\s*([;:`|,!(){}=\-<>\[\]])\s*', r'\1', line)
     return aux
 
+
+def process_define(text):
+    return text
+
+def process_include(text):
+    include_directives = re.compile(r'#include\s*[<](.*?)[>]').findall(text)    
+    include_locals = re.compile(r'#include\s*["](.*?)["]').findall(text)
+    included_any = False
+
+    for match in include_directives:
+        try:
+            filepath = '/usr/include/' + match
+            with open(filepath, 'r') as include_file:
+                include_content = include_file.read()
+                text = text.replace(f'#include <{match}>', include_content)
+                included_any = True
+                include_directives.remove(match)
+        except FileNotFoundError:
+            print(f"Warning: File '{match}' not found in include directive.")            
+
+
+    for match in include_locals:
+        try:
+            with open(match, 'r') as include_file:
+                include_locals.remove(match)
+                include_content = include_file.read()
+                included_any = True
+                text = text.replace(f'#include "{match}"', include_content)
+        except FileNotFoundError:
+            print(f"Warning: File '{match}' not found in local directory.")
+    
+    if (len(include_directives) + len(include_locals)) != 0 and included_any == False:
+        return text
+    else:
+        return process_include(text)
+
+
 def call_error():
     print("Usage: python preCcessor.py <input_file.c>")
     print("The output file with be: <input_file>")
@@ -99,9 +102,9 @@ if __name__ == '__main__':
     if input_file[-2:] != ".c":
         call_error()
 
-    output_file = input_file[0:-2] + "-pp.c"
+    output_file = input_file[0:-2] + "-pcc.c"
 
-    print("preCcessor is running...")
+    print("preCCessor is running...")
     input = open(input_file, "r")
     output = open(output_file, "w")
 
@@ -112,11 +115,12 @@ if __name__ == '__main__':
     aux_code = remove_multiline_comments(aux_code)
     aux_code = remove_multiple_spaces(aux_code)
     aux_code = remove_desnecessary_space(aux_code)
-    aux_code = process_include_directives(aux_code)
+    aux_code = process_include(aux_code)
+    aux_code = process_define(aux_code)
     
     output.write(aux_code)
 
-    print("preCcessor finished with success!")
+    print("preCCessor finished with success!")
 
     input.close()
     output.close()
